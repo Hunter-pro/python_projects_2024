6b01ee18c46e0935624639e1de44a8ae13d9465e
Hunter-pro <101683063+Hunter-pro@users.noreply.github.com>
2024-03-30 22:37:07 +0530

    added env variables

--- automate_mail/main.py
+++ automate_mail/main.py
@@ -3,10 +3,14 @@ import datetime as dt
 from operator import le
 import random
 import smtplib
+import os
+from dotenv import load_dotenv 
+
+load_dotenv('.env')
 
 import pandas
 # 1. Update the birthdays.csv
-my_email = 'kushalcherukula@gmail.com'
+my_email = os.getenv('my_email')
 # 2. Check if today matches a birthday in the birthdays.csv
 now = dt.datetime.now()
 data = pandas.read_csv('./automate_mail/birthdays.csv')
@@ -19,7 +23,7 @@ for birthday in birthdays:
             letter_contents = contents.replace('[NAME]',birthday['name'])
         with smtplib.SMTP('smtp.gmail.com') as connection:
             connection.starttls()
-            connection.login(user=my_email,password='euro ebcq cflo xbhf')
+            connection.login(user=my_email,password=os.getenv('app_password'))
             connection.sendmail(from_addr=my_email,to_addrs=birthday['email'],msg=f'subject:Happy Birthday\n\n {letter_contents}')
 
 
